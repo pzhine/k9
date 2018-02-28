aa689c657098ee4866ce3db07c9744d6ac59c426
Paul Hine <paul.hine@gmail.com>
2018-02-28 23:24:37 +0100

    remove TextInput component

--- src/components/TextInput/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import store from '../../redux/configureStore'
-import TextInput from './'
-
-storiesOf('TextInput', module).add('default', () =>
-  <TextInput store={store} field={'test.input1'} name={'Some Field'} />
-)
