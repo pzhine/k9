b237c624d413af9a7c9a219a09b0c03d23f854ad
Paul Hine <paul.hine@gmail.com>
2018-03-02 02:51:06 +0100

    index all dictionaries on server startup

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-console */
 import express from 'express'
 import controller from './controller'
+import dictionaries from '../../../dictionaries'
+import dictionary from './dictionary'
 
 const router = express.Router()
 
 router.get('/k9/:dict/:numbers', controller)
 router.get('/k9/:numbers', controller)
+
+console.log('BUILDING PREFIX SEARCH INDICIES')
+Object.keys(dictionaries).forEach(lang => dictionary(lang))
 
 export default router
