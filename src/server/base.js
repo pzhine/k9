ef1c0539b2a14887b2360d40ef39281523b3a048
Paul Hine <paul.hine@gmail.com>
2018-03-02 03:51:20 +0100

    add env switch for front/api start

--- src/server/base.js
+++ src/server/base.js
@@ -2,7 +2,6 @@ import express from 'express'
 import compression from 'compression'
 import cors from 'cors'
 import bodyParser from 'body-parser'
-import api from './api'
 
 const server = express()
 
@@ -11,12 +10,15 @@ server.disable('x-powered-by')
 
 // Compress (gzip) assets in production.
 server.use(compression())
-server.use(
-  '/api',
-  cors(),
-  bodyParser.json(),
-  bodyParser.urlencoded({ extended: true }),
-  api
-)
+
+if (process.env.K9_TARGET !== 'FRONT') {
+  server.use(
+    '/api',
+    cors(),
+    bodyParser.json(),
+    bodyParser.urlencoded({ extended: true }),
+    require('./api')
+  )
+}
 
 export default server
